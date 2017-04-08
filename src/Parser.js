4e68888c1b10fafce4243d3cade332d6a3fc0ad0
fluffy <21dmurphy@cr.k12.ia.us>
2017-04-08 18:37:43 -0500

    Don't be a dumbass pls

--- src/Parser.js
+++ src/Parser.js
@@ -13,8 +13,8 @@ class Parser {
 		let commandName = message.content.indexOf(" ") != -1 ? message.content.substring(message.content.indexOf(this.client.prefix) + 1, message.content.indexOf(" ")) : message.content.substring(message.content.indexOf(this.client.prefix) + 1);
 		let params = message.content.split(" ").slice(1);
 		let cmd = this.client.register.commands.find("name", commandName);
-		if (cmd.disabled) return;
-		if (cmd.ownerOnly && message.author.id !== this.client.ownerID) return message.channel.send("This command is owner only.");
+		if (cmd && cmd.disabled) return;
+		if (cmd && cmd.ownerOnly && message.author.id !== this.client.ownerID) return message.channel.send("This command is owner only.");
 		if (cmd) cmd.run(message, params);
 	}
 	
