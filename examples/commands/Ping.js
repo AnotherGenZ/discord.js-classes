0b832be49a93e34f2cd4efde66e8e2909d730756
fluffy <21dmurphy@cr.k12.ia.us>
2017-04-07 21:09:53 -0500

    Example command

---
+++ examples/commands/Ping.js
@@ -0,0 +1,15 @@
+"use strict";
+const { Command } = require("../../index");
+
+exports.command = class Ping extends Command {
+  constructor(client) {
+    super(client, {
+      name: "ping"
+    });
+  }
+
+  async run(message, params) {
+    const msg = await message.channel.send("Ping?");
+    msg.edit(`Pong! ${msg.createdTimestamp - message.createdTimestamp}ms`);
+  }
+}
